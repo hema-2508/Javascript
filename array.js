d0381f7d0133c0442109261c4488c78955d7d12b
hema-2508 <hema.acharya2508@gmail.com>
2025-08-16 12:19:19 +0530

    Practice on javascript

---
+++ array.js
@@ -0,0 +1,31 @@
+// let dress = ["saree", "lehenga", "kurta"];
+
+
+// dress.push("salwar");
+// console.log(dress); 
+
+// let removedDress = dress.pop();
+// console.log(removedDress); 
+
+// console.log(dress);         
+
+
+// dress.unshift("midi");
+// console.log(dress);         
+
+
+
+// let shiftedDress = dress.shift();
+// console.log(shiftedDress);  
+
+// console.log(dress);         
+
+
+
+// let firstDress = dress[0];
+// console.log(firstDress);    
+
+let food=['upma','biriyani','idly'];
+
+let shiftedFood=food.unshift('parotta');
+console.log(shiftedFood);
