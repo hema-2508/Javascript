a6d611ed530cf50470262f0f931880259f5f24b9
hema-2508 <hema.acharya2508@gmail.com>
2025-08-23 12:24:47 +0530

    Practice on javascript

---
+++ example.js
@@ -0,0 +1,159 @@
+const wwePlayers = [
+    {
+        id: 1,
+        name: 'John Cena',
+        brand: 'Raw',
+        height: '6 ft 1 in',
+        weight: '251 lbs',
+        finishers: ['Attitude Adjustment',
+            'STF/STFU'],
+        championships: ['WWE Championship',
+            'World Heavyweight Championship',
+            'United States Championship']
+    },
+    {
+        id: 2,
+        name: 'Roman Reigns',
+        brand: 'SmackDown',
+        height: '6 ft 3 in',
+        weight: '265 lbs',
+        finishers: ['Spear', 
+            'Superman Punch'],
+        championships: ['Universal Championship',
+             'WWE Championship',
+              'Intercontinental Championship']
+    },
+    {
+        id: 3,
+        name: 'The Rock',
+        brand: 'Legend',
+        height: '6 ft 5 in',
+        weight: '260 lbs',
+        finishers: ['Rock Bottom',
+             'Peoples Elbow'],
+        championships: ['WWE Championship', 
+            'WCW Championship', 
+            'Intercontinental Championship']
+    },
+    {
+        id: 4,
+        name: 'Seth Rollins',
+        brand: 'Raw',
+        height: '6 ft 1 in',
+        weight: '217 lbs',
+        finishers: ['Stomp', 
+            'Pedigree', 
+            'Superkick'],
+        championships: ['WWE Championship', 
+            'Universal Championship', 
+            'United States Championship']
+    },
+    {
+        id: 5,
+        name: 'CM Punk',
+        brand: 'Raw',
+        height: '6 ft 2 in',
+        weight: '218 lbs',
+        finishers: ['GTS', 
+            'Anaconda Vise'],
+        championships: ['WWE Championship', 
+            'World Heavyweight Championship',
+             'Intercontinental Championship']
+    },
+    {
+        id: 6,
+        name: 'Triple H',
+        brand: 'Legend',
+        height: '6 ft 4 in',
+        weight: '255 lbs',
+        finishers: ['Pedigree',
+             'Sledgehammer'],
+        championships: ['WWE Championship',
+             'World Heavyweight Championship',
+              'Intercontinental Championship']
+    },
+    {
+        id: 7,
+        name: 'Drew McIntyre',
+        brand: 'SmackDown',
+        height: '6 ft 5 in',
+        weight: '265 lbs',
+        finishers: ['Claymore Kick',
+             'Future Shock DDT'],
+        championships: ['WWE Championship', 
+            'Intercontinental Championship', 
+            'Raw Tag Team Championship']
+    },
+    {
+        id: 8,
+        name: 'Cody Rhodes',
+        brand: 'SmackDown',
+        height: '6 ft 2 in',
+        weight: '220 lbs',
+        finishers: ['Cross Rhodes', 'Cody Cutter'],
+        championships: ['WWE Championship', 'Intercontinental Championship', 'Tag Team Championship']
+    },
+    {
+        id: 9,
+        name: 'The Undertaker',
+        brand: 'Legend',
+        height: '6 ft 10 in',
+        weight: '309 lbs',
+        finishers: ['Tombstone Piledriver', 'Last Ride', 'Hells Gate'],
+        championships: ['WWE Championship', 'World Heavyweight Championship', 'Hardcore Championship']
+    },
+    {
+        id: 10,
+        name: 'Brock Lesnar',
+        brand: 'Free Agent',
+        height: '6 ft 3 in',
+        weight: '286 lbs',
+        finishers: ['F5', 'Kimura Lock'],
+        championships: ['WWE Championship', 'Universal Championship', 'UFC Heavyweight Championship']
+    },
+    {
+        id: 11,
+        name: 'Stone Cold',
+        brand: 'Legend',
+        height: '6 ft 2 in',
+        weight: '252 lbs',
+        finishers: ['Stone Cold Stunner', 'Lou Thesz Press'],
+        championships: ['WWE Championship', 'Intercontinental Championship', 'Tag Team Championship']
+    },
+    {
+        id: 12,
+        name: 'Gunther',
+        brand: 'Raw',
+        height: '6 ft 4 in',
+        weight: '317 lbs',
+        finishers: ['Powerbomb', 'Sleeper Hold'],
+        championships: ['Intercontinental Championship', 'NXT UK Championship', 'wXw Unified World Wrestling Championship']
+    },
+    {
+        id: 13,
+        name: 'Kevin Owens',
+        brand: 'SmackDown',
+        height: '6 ft 0 in',
+        weight: '266 lbs',
+        finishers: ['Pop-up Powerbomb', 'Stunner'],
+        championships: ['Universal Championship', 'Intercontinental Championship', 'United States Championship']
+    },
+    {
+        id: 14,
+        name: 'Randy Orton',
+        brand: 'SmackDown',
+        height: '6 ft 5 in',
+        weight: '250 lbs',
+        finishers: ['RKO', 'Punt Kick'],
+        championships: ['WWE Championship', 'World Heavyweight Championship', 'Intercontinental Championship']
+    },
+    {
+        id: 15,
+        name: 'LA Knight',
+        brand: 'SmackDown',
+        height: '6 ft 0 in',
+        weight: '234 lbs',
+        finishers: ['BFT', 'Megastar Kick'],
+        championships: ['United States Championship', 'NXT Championship', 'Million Dollar Championship']
+    }
+];
